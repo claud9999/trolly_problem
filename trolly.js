c9dd2b7f33b23d7ce6384158df1fc91bfa964d6d
Chris Knight <cdk-github@accessoft.org>
2023-03-08 10:41:14 -0800

    improved rail building

--- trolly.js
+++ trolly.js
@@ -6,9 +6,13 @@ direction =
     128 = left
 */
 
-class Switch {
+class Point {
+    constructor(x, y) { this.x = x; this.y = y; }
+}
+
+class Switch extends Point {
     constructor(x, y, direction, map) {
-        this.x = x; this.y = y; this.direction = direction;
+        super(x, y); this.direction = direction;
         this.map = map;
     }
 
@@ -17,98 +21,139 @@ class Switch {
         this.map.ctx.lineWidth = 3;
         this.map.ctx.strokeStyle = "red";
         this.map.ctx.moveTo(this.x * this.map.cell_width, this.y * this.map.cell_height);
-        this.map.ctx.lineTo((this.x + this.map.delta[this.direction][0]) * this.map.cell_width, (this.y + this.map.delta[this.direction][1]) * this.map.cell_height);
+        this.map.ctx.lineTo((this.x + this.map.delta[this.direction].x) * this.map.cell_width, (this.y + this.map.delta[this.direction].y) * this.map.cell_height);
         this.map.ctx.stroke();
     }
 }
-class NPC {
+class NPC extends Point {
     constructor(emoji, x, y, map) {
+        super(x, y);
         this.map = map;
         this.emoji = emoji;
-        this.x = x;
-        this.y = y;
     }
 
     paint() {
         this.map.ctx.fillText(this.emoji, this.x * this.map.cell_width, this.y * this.map.cell_height);
     }
 }
+
+class RailSegment extends Point {
+    constructor(x, y, d) {
+        super(x, y);
+        this.d = d; this.l = 0;
+    }
+}
+
 class Rail {
-    constructor(map) {
-        this.map = map;
-        this.points = [];
+    build() {
         var x = 0;
         var y = 0;
-        var cell_count = 0;
+        var l = 0;
+        this.points = new Array();
+
         /* pick edge to start from, 0 = bottom, 1 = top, 2 = left, 3 = right */
-        var direction = Math.floor(Math.random() * 4);
-        var prevdir = -1;
-        switch(direction) {
+        var d = Math.floor(Math.random() * 4);
+        switch(d) {
             case 0: /* heading up (+ left or right) */
-                direction = Math.floor(Math.random() * 3);
-                y = map.height;
-                x = Math.floor((Math.random() + Math.random()) / 2 * map.width);
+                d = Math.floor(Math.random() * 3);
+                y = this.map.height - 1;
+                x = Math.floor((Math.random() + Math.random()) / 2 * this.map.width);
                 break;
             case 1: /* heading down (+ left or right) */
-                direction = Math.floor(Math.random() * 3) + 4;
-                y = -1;
-                x = Math.floor((Math.random() + Math.random()) / 2 * map.width);
+                d = Math.floor(Math.random() * 3) + 4;
+                y = 0;
+                x = Math.floor((Math.random() + Math.random()) / 2 * this.map.width);
                 break;
             case 2: /* heading right (+ up or down) */
-                direction = Math.floor(Math.random() * 3) + 2;
-                x = -1;
-                y = Math.floor((Math.random() + Math.random()) / 2 * map.height);
+                d = Math.floor(Math.random() * 3) + 2;
+                x = 0;
+                y = Math.floor((Math.random() + Math.random()) / 2 * this.map.height);
                 break;
             case 3: /* heading left (+ up or down) */
-                direction = (Math.floor(Math.random() * 3) + 6) % 8;
-                x = map.width;
-                y = Math.floor((Math.random() + Math.random()) / 2 * map.height);
+                d = (Math.floor(Math.random() * 3) + 6) % 8;
+                x = this.map.width - 1;
+                y = Math.floor((Math.random() + Math.random()) / 2 * this.map.height);
                 break;
         }
-        this.points.push([x, y]);
+        var segment = new RailSegment(x, y, d);
+        this.points.push(segment);
 
         while(1) {
-            if (prevdir >= 0) { /* mark leaving path */
-                map.railcells[x + y * map.width] |= map.bitmap[(prevdir + 4) % 8];
+            x += this.map.delta[d].x;
+            y += this.map.delta[d].y;
+            segment.l++;
+            l++;
+            if (x < 0 || x > this.map.width - 1 || y < 0 || y > this.map.height - 1) {
+                segment.l--;
+                break;
             }
-            x += map.delta[direction][0];
-            y += map.delta[direction][1];
-            if (x < 0 || x >= map.width || y < 0 || y >= map.height) break;
-            if (map.terminus[direction] & map.railcells[x + y * map.width]) {
+            if (this.map.terminus[d] & this.map.railcells[x][y]) {
                 break; /* there are tracks going (basically) the same direction */
             }
-            map.railcells[x + y * map.width] |= map.bitmap[direction]; /* mark entering path */
-            if (cell_count > 3) { // don't start turning for a few steps into the map */
+            if (l > 3) { // don't start turning for a few steps into the map */
                 var turn = Math.random();
                 if (turn > 0.9 && turn < 0.95) { /* turn left */
-                    this.points.push([x, y]);
-                    direction--; if (direction < 0) direction = 7;
+                    d--; if (d < 0) d = 7;
+                    segment = new RailSegment(x, y, d);
+                    this.points.push(segment);
                 } else if (turn >= 0.95) {
-                    this.points.push([x, y]);
-                    direction++; if (direction > 7) direction = 0;
+                    d++; if (d > 7) d = 0;
+                    segment = new RailSegment(x, y, d);
+                    this.points.push(segment);
                 }
             }
-            cell_count++;
-            prevdir = direction;
         }
-        map.railcells[x + y * map.width] |= map.bitmap[(prevdir + 4) % 8]; /* mark entering path */
-        this.points.push([x, y]);
+
+        /* too short, try again */
+        if(l < 10) return this.build();
+    }
+
+    mark() {
+        for (var n = 0; n < this.points.length; n++) {
+            var p = this.points[n];
+            var x = p.x; var y = p.y; var d = p.d;
+            for(var l = 0; l < p.l; l++) {
+                // mark outgoing
+                this.map.railcells[x][y] |= this.map.bitmap[(d + 4) % 8];
+                x += this.map.delta[d].x;
+                y += this.map.delta[d].y;
+                // mark incoming
+                this.map.railcells[x][y] |= this.map.bitmap[d];
+            }
+        }
+    }
+
+    constructor(map) {
+        this.map = map;
+        this.build();
+        this.mark();
     }
 
     paint() {
         this.map.ctx.beginPath();
-        this.map.ctx.moveTo(this.points[0][0] * this.map.cell_width, this.points[0][1] * this.map.cell_height);
-        for(var i = 1; i < this.points.length; i++) {
-            this.map.ctx.lineTo(this.points[i][0] * this.map.cell_width, this.points[i][1] * this.map.cell_height);
-        };
+        var i = 1;
+        var p = this.points[0];
+        var m = this.map;
+        m.ctx.moveTo(p.x * m.cell_width,
+            p.y * m.cell_height);
+        for(i = 1; i < this.points.length; i++) {
+            p = this.points[i];
+            m.ctx.lineTo(
+                p.x * m.cell_width,
+                p.y * m.cell_height);
+        }
+        m.ctx.lineTo(
+            (p.x + m.delta[p.d].x * p.l) * m.cell_width,
+            (p.y + m.delta[p.d].y * p.l) * m.cell_height
+            );
 
-        this.map.ctx.lineWidth = 6;
-        this.map.ctx.strokeStyle = "black";
-        this.map.ctx.stroke();
+        m.ctx.lineWidth = 6;
+        m.ctx.strokeStyle = "black";
+        m.ctx.stroke();
 
-        this.map.ctx.lineWidth = 3;
-        this.map.ctx.strokeStyle = "white";
-        this.map.ctx.stroke();
+        m.ctx.lineWidth = 3;
+        m.ctx.strokeStyle = "white";
+        m.ctx.stroke();
      }
 }
 
@@ -126,9 +171,22 @@ class RailMap {
         this.cell_height = meas.actualBoundingBoxAscent;
         this.width = Math.floor(this.pixel_width / this.cell_width);
         this.height = Math.floor(this.pixel_height / this.cell_height);
-        this.railcells = new Array(this.width * this.height);
+        this.railcells = new Array(this.width);
+        for(var x = 0; x < this.width; x++) {
+            this.railcells[x] = new Array(this.height);
+            for(var y = 0; y < this.height; y++) this.railcells[x][y] = 0;
+        }
         this.switches = new Array();
-        this.delta = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
+        this.delta = [
+            new Point(-1, -1), // up + left
+            new Point(0, -1), // up
+            new Point(1, -1), // up + right
+            new Point(1, 0), // right
+            new Point(1, 1), // down + right
+            new Point(0, 1), // down
+            new Point(-1, 1), // down + left
+            new Point(-1, 0) // left
+        ];
         this.bitmap = [1, 2, 4, 8, 16, 32, 64, 128];
         this.terminus = [
             /* for a given direction, if there are outgoing in one of these directions, stop */
@@ -141,9 +199,6 @@ class RailMap {
             224,  /* down, down + left, left */
             193 /* down + left, left, up + left */
         ];
-        for (var i = 0; i < this.railcells.length; i++) {
-            this.railcells[i] = 0;
-        }
 
         this.lines = [];
         this.npcs = [];
@@ -176,11 +231,12 @@ class RailMap {
     addSwitches() {
         for(var x = 0; x < this.width; x++) {
             for (var y = 0; y < this.height; y++) {
-                var c = this.railcells[x + y * this.width];
+                var c = this.railcells[x][y];
                 if (c == 0) continue; // shortcut empty cells
                 for (var b = 0; b < 8; b++) {
                     if (c & (2 ** b)) {
                         if (c & (2 ** ((b + 1) % 8))) {
+                            console.log([c.toString(2),x,y,b]);
                             this.addSwitch(x, y, b);
                             break;
                         }
@@ -195,11 +251,13 @@ function kp(event) {
     console.log(event.keyCode);
 }
 
+var rm;
+
 function trolly() {
     var c = document.getElementById("trolly");
     window.onkeydown = kp;
 
-    var rm = new RailMap();
+    rm = new RailMap();
 
     for(var i = 0; i < 20; i++) {
         rm.add();
