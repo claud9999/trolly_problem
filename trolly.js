9d7bbc0f81d6ff3f44b5e64ab97a6eb073f96edf
Chris Knight <cdk-github@accessoft.org>
2023-03-09 10:06:01 -0800

    lll

--- trolly.js
+++ trolly.js
@@ -6,6 +6,10 @@ d =
     128 = left
 */
 
+var game = false;
+
+var score = 0;
+
 class Point {
     constructor(x, y) { this.x = x; this.y = y; }
 }
@@ -19,7 +23,7 @@ class Switch extends Point {
     paint(ctx) {
         ctx.beginPath();
         ctx.lineWidth = 3;
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = "green";
         ctx.moveTo(this.x * this.map.cell_width, this.y * this.map.cell_height);
         ctx.lineTo((this.x + this.map.delta[this.d].x) * this.map.cell_width, (this.y + this.map.delta[this.d].y) * this.map.cell_height);
         ctx.stroke();
@@ -41,20 +45,83 @@ class Token extends Point {
 }
 
 class NPC extends Token {
-    constructor(emoji, x, y, map) {
+    constructor(emoji, x, y, map, speed) {
         super(emoji, x, y, map);
+        this.count = Math.floor(Math.random() * speed);
+        this.speed = speed;
+        this.d = Math.floor(Math.random() * 8);
+    }
+
+    // if my speed is 1, I will move every 20 turns...
+    // if my speed is 15, I'll move on turn 2, 3, 5, 6, 8, 9...
+    tick() {
+        this.count += this.speed;
     }
 
     move() {
-        x += Math.floor(Math.random() * 3) - 1;
-        y += Math.floor(Math.random() * 3) - 1;
-        return (x >= 0 && y >= 0 && x < this.map.width && y < this.map.height);
+        if (this.count < 20) return true;
+        this.count -= 20;
+
+        this.x += this.map.delta[this.d].x;
+        this.y += this.map.delta[this.d].y;
+        return (this.x >= 0 && this.y >= 0 && this.x < this.map.width && this.y < this.map.height);
     }
 }
 
 class PC extends Token {
     constructor(emoji, x, y, map) {
         super(emoji, x, y, map);
+    }
+
+    move(d) {
+        const dx = this.map.delta[d].x;
+        const dy = this.map.delta[d].y;
+        this.x += dx;
+        this.y += dy;
+
+        // no going off the screen
+        if (this.x < 0 || this.y < 0 || this.x >= this.map.width || this.y >= this.map.height) {
+            this.x -= dx;
+            this.y -= dy;
+            return true;
+        }
+
+        // push an NPC
+        for (var i = 0; i < this.map.npcs.length; i++) {
+            const npc = this.map.npcs[i];
+            if (npc.x == this.x && npc.y == this.y) {
+                npc.x += dx;
+                npc.y += dy;
+                npc.count = 0;
+                for(var d = 0; d < this.map.delta.length; d++) {
+                    if (dx == this.map.delta[d].x && dy == this.map.delta[d].y) {
+                        npc.d = d;
+                        break;
+                    }
+                }
+
+                // can't push them off the screen
+                if (npc.x < 0 || npc.y < 0 || npc.x >= this.map.width || npc.y >= this.map.height) {
+                    this.x -= dx;
+                    this.y -= dy;
+                    npc.x -= dx;
+                    npc.y -= dy;
+                    return true;
+                }
+            }
+        };
+
+        // can't move through a train
+        for (i = 0; i < this.map.trains.length; i++) {
+            const train = this.map.trains[i];
+            if (train.x == this.x && train.y == this.y) {
+                this.x -= dx;
+                this.y -= dy;
+                return true;
+            }
+        }
+
+        return true;
     }
 }
 
@@ -70,35 +137,34 @@ class Train extends Token {
     }
 
     tick() {
-        this.remaining = this.speed;
+        this.remaining += this.speed;
     }
 
     move() {
         if (this.remaining <= 0) return true;
         this.remaining--;
 
+        var d = -1, td = 0;
+        const c = this.map.railcells[this.x][this.y];
+
         // check if I'm at a switch and can take it
-        var d = -1;
         for (var s = 0; d < 0 && s < this.map.switches.length; s++) {
             var sw = this.map.switches[s];
             if (sw.x != this.x || sw.y != this.y) continue;
-            var td = this.d - 1; if (td < 0) td = 7;
-            if (td == sw.d || ((this.d + 1) % 8) == sw.d) d = sw.d;
+            td = this.d - 1; if (td < 0) td = 7;
+            if (td == sw.d || (this.d + 1) % 8 == sw.d) d = sw.d;
         }
 
         // proceed forward, if possible
-        const c = this.map.railcells[this.x][this.y];
         if (d < 0 && c & (2 ** this.d)) d = this.d;
 
         // take a branch, if possible
         if (d < 0) {
-            var td = 0;
             if(Math.random() > 0.5) {
                 // try left first
-                td = this.d - 1;
-                if (td < 0) td = 7;
+                td = this.d - 1; if (td < 0) td = 7;
                 if (c & (2 ** td)) d = td;
-                // then right
+                // then try right
                 else {
                     td = (this.d + 1) % 8;
                     if (c & (2 ** td)) d = td;
@@ -109,8 +175,7 @@ class Train extends Token {
                 if (c & (2 ** td)) d = td;
                 // then left
                 else {
-                    td = this.d - 1;
-                    if (td < 0) td = 7;
+                    td = this.d - 1; if (td < 0) td = 7;
                     if (c & (2 ** td)) d = td;
                 }
             }
@@ -122,6 +187,28 @@ class Train extends Token {
 
         this.x += this.map.delta[d].x;
         this.y += this.map.delta[d].y;
+        if(this.x < 0 || this.y < 0 || this.x >= this.map.width || this.y >= this.map.height) return false;
+
+        for(var i = 0; i < this.map.trains.length; i++) {
+            const tr = this.map.trains[i];
+            // another train going a different direction, I explode
+            // TODO: add explosion
+            if (tr.x == this.x && tr.y == this.y && tr.d != this.d) return false;
+        }
+
+        for (i = 0; i < this.map.npcs.length; i++) {
+            const npc = this.map.npcs[i];
+            if (npc.x == this.x && npc.y == this.y) {
+                score++;
+                // TODO: create *splat*
+                this.map.npcs.splice(i--, 1);
+                this.map.addNPC();
+            }
+        }
+
+        if (this.map.pc.x == this.x && this.map.pc.y == this.y) {
+            gameover();
+        }
 
         return true;
     }
@@ -134,7 +221,18 @@ class RailSegment extends Point {
     }
 }
 
-class Rail {
+class RailLine {
+    constructor(map) {
+        this.map = map;
+        this.c = linecolors[Math.floor(Math.random() * linecolors.length)];
+        this.build();
+        this.mark();
+    }
+
+    // starting from one edge, start plotting out the route...
+    // turning, occasionally (but not until after moving a few spaces in)
+    // and joining any tracks that this one runs into at a 45-degree.
+    // ...otherwise terminating when I reach a screen edge.
     build() {
         var x = 0;
         var y = 0;
@@ -178,7 +276,6 @@ class Rail {
                 break;
             }
             if (this.map.terminus[d] & this.map.railcells[x][y]) {
-                console.log(["TERM", x, y, d]);
                 break; /* there are tracks going (basically) the same d */
             }
             if (l > 3) { // don't start turning for a few steps into the map */
@@ -199,6 +296,11 @@ class Rail {
         if(l < 10) return this.build();
     }
 
+    // In order to map out where the rail lines are, I keep an
+    // array with each cell having an integer bitmap of the
+    // directions tracks are heading out of the cell. Once
+    // I've run "build()", then I go through and mark the cells
+    // so that later rail lines can intersect this one.
     mark() {
         for (var n = 0; n < this.points.length; n++) {
             var p = this.points[n];
@@ -214,12 +316,7 @@ class Rail {
         }
     }
 
-    constructor(map) {
-        this.map = map;
-        this.build();
-        this.mark();
-    }
-
+    // Draw this rail on the canvas.
     paint(ctx) {
         ctx.beginPath();
         const m = this.map;
@@ -239,7 +336,7 @@ class Rail {
             );
 
         ctx.lineWidth = 6;
-        ctx.strokeStyle = "black";
+        ctx.strokeStyle = this.c;
         ctx.stroke();
 
         ctx.lineWidth = 3;
@@ -248,13 +345,14 @@ class Rail {
      }
 }
 
+// the "master" class, containing all rail lines, NPC's, trains, and canvas details.
 class RailMap {
-    constructor() {
-        this.canvas = document.getElementById("trolly");
+    constructor(canvas) {
+        this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
-        this.pixel_width = window.innerWidth - 20;
+        this.pixel_width = window.innerWidth - 10;
         this.canvas.width = this.pixel_width;
-        this.pixel_height = window.innerHeight - 20;
+        this.pixel_height = window.innerHeight - 10;
         this.canvas.height = this.pixel_height;
 
         var meas = this.ctx.measureText("\u{1F600}");
@@ -293,26 +391,25 @@ class RailMap {
         this.lines = [];
         this.npcs = [];
         this.trains = [];
-
-        this.pc = new PC("\u{1F479}", Math.floor(this.width / 2), Math.floor(this.height / 2), this);
     }
 
-    add() {
-        this.lines.push(new Rail(this));
+    addLine() {
+        this.lines.push(new RailLine(this));
     }
 
     paint() {
-        this.ctx.clearRect(0,0, this.pixel_width, this.pixel_height);
+        this.ctx.clearRect(0, 0, this.pixel_width, this.pixel_height);
 
         for(var i = 0; i < this.lines.length; i++) {
             this.lines[i].paint(this.ctx);
         };
 
-        for (var i = 0; i < this.npcs.length; i++) {
-            this.npcs[i].paint(this.ctx);
-        }
         for (i = 0; i < this.switches.length; i++) {
             this.switches[i].paint(this.ctx);
+        }
+
+        for (var i = 0; i < this.npcs.length; i++) {
+            this.npcs[i].paint(this.ctx);
         }
 
         for (i = 0; i < this.trains.length; i++) {
@@ -320,10 +417,20 @@ class RailMap {
         }
 
         this.pc.paint(this.ctx);
+
+        this.ctx.fillText("score: " + score, this.pixel_width - 100, 20);
     }
 
     addNPC() {
-        this.npcs.push(new NPC(emojis.substring(0,2), Math.floor(Math.random() * this.width), Math.floor(Math.random() * this.height), this));
+        // create an NPC, their speed will be score-dependent
+        // (later NPCs will move faster, initial ones will move very slowly.)
+        var e = Math.floor(Math.random() * emojis.length / 2) * 2;
+        this.npcs.push(new NPC(
+            emojis.substring(e, e + 2),
+            Math.floor(Math.random() * this.width),
+            Math.floor(Math.random() * this.height),
+            this,
+            Math.floor(Math.random() * 3) + score));
     }
 
     addSwitch(x, y, d) {
@@ -338,7 +445,6 @@ class RailMap {
                 for (var d = 0; d < 8; d++) {
                     if (c & (2 ** d)) {
                         if (c & (2 ** ((d + 1) % 8))) {
-                            console.log([c.toString(2), x, y, d]);
                             this.addSwitch(x, y, d);
                             break;
                         }
@@ -350,83 +456,139 @@ class RailMap {
 
     addTrain() {
         var railnum = Math.floor(Math.random() * this.lines.length);
-        this.trains.push(new Train(this.lines[railnum], Math.floor(Math.random() * 6)));
+        this.trains.push(new Train(this.lines[railnum], Math.floor(Math.random() * 3) + score + 1));
     }
 
+    // returns true if there is a switch here and I've updated it...
     toggleSwitch(x, y) {
         var s = 0;
         for (s = 0; s < this.switches.length; s++) {
             if (this.switches[s].x == x && this.switches[s].y == y) break;
         }
-        if(s == this.switches.length) return;
+        if(s == this.switches.length) return false;
         s = this.switches[s];
         for(var d = (s.d + 1) % 8; d != s.d; d = (d + 1) % 8) {
             if (this.railcells[x][y] & (2 ** d)) break;
         }
         s.d = d;
+        return true;
     }
 }
 
+var intervalID = false;
 function kp(event) {
-    console.log(event.keyCode);
+    var d = -1;
     switch(event.keyCode) {
-        case 37: // left
-        case 65:
-            rm.pc.x--;
-            break;
+        case 81: // up + left
+            d = 0; break;
         case 38: // up
         case 87:
-            rm.pc.y--;
-            break;
+            d = 1; break;
+        case 69: // up + right
+            d = 2; break;
         case 39: // right
         case 68:
-            rm.pc.x++;
-            break;
+            d = 3; break;
+        case 67: // down + right
+            d = 4; break;
         case 40: // down
+        case 88:
+            d = 5; break;
+        case 90: // down + left
+            d = 6; break;
+        case 37: // left
+        case 65:
+            d = 7; break;
+        case 32: // switch
         case 83:
-            rm.pc.y++;
+            if(!game.toggleSwitch(game.pc.x, game.pc.y)) return;
             break;
-        case 32: // spacebar
-            rm.toggleSwitch(rm.pc.x, rm.pc.y);
+        default:
+            return;
     }
-    if(rm.trains.length < 4) rm.addTrain();
 
-/*    for(var i = 0; i < rm.npcs.length; i++) {
-        if(!rm.npcs[i].move()) rm.npcs.splice(i--,1);
-    }
-    */
+    if(intervalID != false) clearInterval(intervalID);
+    intervalID = false;
 
-    for(i = 0; i < rm.trains.length; i++) {
-        rm.trains[i].tick();
+    if (d >= 0) game.pc.move(d);
+
+    // when trains disappear/crash, replace them.
+    // also increase train density as score goes up.
+    if(game.trains.length < score + 4) game.addTrain();
+
+    // let the trains move since I've moved...
+    for(i = 0; i < game.trains.length; i++) {
+        game.trains[i].tick();
     }
+
+    // let the NPCs move since I've moved...
+    for(i = 0; i < game.npcs.length; i++) {
+        game.npcs[i].tick();
+    }
+
+    game.paint();
+
+    intervalID = setInterval(doUpdates, 200);
 }
 
-var rm;
-
-function process() {
-    for(i = 0; i < rm.trains.length; i++) {
-        if(!rm.trains[i].move()) rm.trains.splice(i--,1);
+function doUpdates() {
+    for(var i = 0; i < game.trains.length; i++) {
+        if(!game.trains[i].move()) game.trains.splice(i--,1);
     }
-    rm.paint();
+
+    for(i = 0; i < game.npcs.length; i++) {
+        if(!game.npcs[i].move()) {
+            game.npcs.splice(i--,1);
+            game.addNPC();
+        }
+    }
+
+    game.paint();
 }
 
 function trolly() {
     var c = document.getElementById("trolly");
     window.onkeydown = kp;
+    score = 0;
 
-    rm = new RailMap();
+    game = new RailMap(c);
 
-    for(var i = 0; i < 10; i++) {
-        rm.add();
+    game.pc = new PC("\u{1F479}", Math.floor(game.width / 2), Math.floor(game.height / 2), game);
+
+    // add a fixed-number of rail lines
+    // TODO: perhaps this should be canvas size-dependent?
+    for(var i = 0; i < 15; i++) {
+        game.addLine();
     };
 
     for(i = 0; i < 10; i ++) {
-        rm.addNPC();
+        game.addNPC();
     }
 
-    rm.addSwitches();
+    game.addSwitches();
 
-    rm.paint();
+    game.paint();
+}
 
-    window.setInterval(process, 100);
+function gameover() {
+    var c = document.getElementById("trolly");
+    var g = document.getElementById("gameover");
+    if(!c.hidden) {
+        c.hidden = true;
+        g.hidden = false;
+        var s = document.getElementById("score");
+        s.innerText = score;
+    }
+}
+
+function showgame() {
+    var c = document.getElementById("trolly");
+    var i = document.getElementById("instructions");
+    if(c.hidden) {
+        i.hidden = true;
+        c.hidden = false;
+        c.width = "100%";
+        c.height = "100%";
+        trolly();
+    }
 }
